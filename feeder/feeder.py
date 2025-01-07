3a09cac4b848d1953c7573e5b848b04572ec914a
cheny6 <2844246921@qq.com>
2025-01-07 17:28:19 +0800

    Added unsupervised feeders for label and device, and merged the three feeders to add other unsupervised configs

--- feeder/feeder.py
+++ feeder/feeder.py
@@ -2,26 +2,23 @@ import torch
 import random
 import pickle
 import numpy as np
+from. import augmentations
 
-from . import augmentations
 
-class Feeder_snr(torch.utils.data.Dataset):
-    """ Feeder for label inputs """
-
+class BaseFeeder(torch.utils.data.Dataset):
+    """ Base Feeder for label inputs """
     def __init__(self, data_path, mode):
         self.data_path = data_path
         self.mode = mode
         self.load_data()
-    
+
     def load_data(self):
         # data: mfcc snr device label
         with open(self.data_path, 'rb') as file:
             data = pickle.load(file)
-        
         self.data = []
         self.label = []
-
-        for i,snr in enumerate(data):
+        for i, snr in enumerate(data):
             for j in snr:
                 self.data.append(j)
                 self.label.append(i)
@@ -33,7 +30,6 @@ class Feeder_snr(torch.utils.data.Dataset):
         # get data label
         data = self.data[index][0]
         label = self.label[index]
-        
         if self.mode == 'single':
             return data, label
         elif self.mode == 'double':
@@ -55,42 +51,17 @@ class Feeder_snr(torch.utils.data.Dataset):
             data = augmentations.time_warp(data)
         return augmentations.crop_or_pad(data)
 
-class Feeder_device(torch.utils.data.Dataset):
+
+class Feeder_snr(BaseFeeder):
+    """ Feeder for snr inputs """
+    pass
+
+
+class Feeder_device(BaseFeeder):
+    """ Feeder for device inputs """
+    pass
+
+
+class Feeder_label(BaseFeeder):
     """ Feeder for label inputs """
-
-    def __init__(self, data_path, snr):
-        self.data_path = data_path
-        self.snr = snr
-        self.load_data()
-    
-    def load_data(self):
-        # data: mfcc snr device label
-        with open(self.data_path, 'rb') as file:
-            self.data = pickle.load(file)
-        
-    def __len__(self):
-        return len(self.data[self.snr])
-
-    def __getitem__(self, index):
-        # get data
-        return self.data[self.snr][index][0], self.data[self.snr][index][2]
-
-class Feeder_label(torch.utils.data.Dataset):
-    """ Feeder for label inputs """
-
-    def __init__(self, data_path, snr):
-        self.data_path = data_path
-        self.snr = snr
-        self.load_data()
-    
-    def load_data(self):
-        # data: mfcc snr device label
-        with open(self.data_path, 'rb') as file:
-            self.data = pickle.load(file)
-        
-    def __len__(self):
-        return len(self.data[self.snr])
-
-    def __getitem__(self, index):
-        # get data
-        return self.data[self.snr][index][0], self.data[self.snr][index][2]
+    pass
